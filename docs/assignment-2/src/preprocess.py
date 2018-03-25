acf7118f529df6bb549434973f77537966e4032c
Ali Gholami <aligholami7596@gmail.com>
2018-03-26 02:03:38 +0430

    Performs standardization on training data

--- docs/assignment-2/src/preprocess.py
+++ docs/assignment-2/src/preprocess.py
@@ -2,6 +2,7 @@ import numpy as np
 import pprint 
 import pandas as pd
 from collections import deque
+from sklearn.preprocessing import StandardScaler
 
 # Define root data path
 DATA_ROOT = './data/'
@@ -99,8 +100,20 @@ train_data.col_8 = train_data.col_8.astype(float)       # Mean does not work for
 train_data['col_8'].fillna(train_data['col_8'].mean(), inplace=True)
 print(train_data.dtypes)
 
+# Bring out the training labels before anything stupid happens
+separate_output('Separated Training Labels')
+train_labels = train_data['col_39']
+train_data = train_data.drop(['col_39'], axis=1)
+print(train_labels)
+
 # Convert the categorical data to the numeric form
 # Select the columns with object type
 separate_output('Converted Data to Numeric Format')
 train_data = obj_to_num(train_data)
 print(train_data)
+
+# Standardize the data using Standard Scaler
+# Standardize the data along each column
+separate_output('Standardized Train Data')
+train_data = StandardScaler().fit_transform(train_data)
+print(train_data.shape)
