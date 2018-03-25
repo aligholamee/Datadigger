1247124a3d2cd27e89e79bf18139568ac479f781
Ali Gholami <aligholami7596@gmail.com>
2018-03-25 18:05:18 +0430

    Generates new column names :)

---
+++ docs/assignment-2/src/preprocess.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+DATA_ROOT = './data/'
+
+train_data = pd.read_csv(DATA_ROOT + 'train.csv')
+test_data = pd.read_csv(DATA_ROOT + 'test.csv')
+
+# Generate column names
+COLUMN_NAMES = ['col_' + str(i+1) for i in range(train_data.shape[1])]
+
+# Rename dataframe columns
+train_data.columns = COLUMN_NAMES
+
+# Get datatypes
+print(train_data.dtypes)
