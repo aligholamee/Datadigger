240ff72c04b36400289e992d02335e69754fb492
Ali Gholami <aligholami7596@gmail.com>
2018-03-26 12:35:32 +0430

    Added graph

--- docs/assignment-2/src/model.py
+++ docs/assignment-2/src/model.py
@@ -143,3 +143,5 @@ export_graphviz(decision_tree, out_file=dot_data,
                 special_characters=True)
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
 Image(graph.create_png())
+
+
