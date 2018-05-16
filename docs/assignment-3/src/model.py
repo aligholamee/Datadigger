360498a1330e6061346cdc85d5a62cb3a3f793f6
Ali Gholami <aligholami7596@gmail.com>
2018-05-16 21:07:17 +0430

    Update: Transform features into the TF-IDF mode

--- docs/assignment-3/src/model.py
+++ docs/assignment-3/src/model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-# from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 DATA_ROOT = './data/'
 
@@ -15,7 +15,17 @@ def extract_labels(data):
 dataset = pd.read_csv(DATA_ROOT + 'spam.csv')
 train_data, train_labels = extract_labels(dataset)
 
-print(train_data)
+# Extract features using Bag of Words model
+count_vect = CountVectorizer()
+X_train = count_vect.fit_transform(train_data)
+
+# Enable TF-IDF
+tf_idf = TfidfTransformer()
+X_train = tf_idf.fit_transform(X_train)
+print(X_train.shape)
+
+
+
 
 
 
