0cb0749923f7b41db17f1f0890d5057b1a4bec2b
Ali Gholami <aligholami7596@gmail.com>
2018-05-16 20:21:33 +0430

    Update: Extract the data and separate the labels

--- docs/assignment-3/src/model.py
+++ docs/assignment-3/src/model.py
@@ -1,11 +1,21 @@
 import pandas as pd
-
+# from sklearn.feature_extraction.text import CountVectorizer
 
 DATA_ROOT = './data/'
 
 
+def extract_labels(data):
+    """
+        Extracts raw data and Its labels
+    """
+    
+    return (dataset['v2'], data['v1'])
+    
 # Extract the data and separate the labels
 dataset = pd.read_csv(DATA_ROOT + 'spam.csv')
-train_labels = dataset['v1']
-train_data = dataset['v2']
+train_data, train_labels = extract_labels(dataset)
+
+print(train_data)
+
+
 
