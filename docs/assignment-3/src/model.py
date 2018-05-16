7b4471a901ca04a2c780fb2364896559ddd26ecd
Ali Gholami <aligholami7596@gmail.com>
2018-05-16 21:46:48 +0430

    Update: Trains the NaiveBayes classifier

--- docs/assignment-3/src/model.py
+++ docs/assignment-3/src/model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
 
 DATA_ROOT = './data/'
 
@@ -15,14 +17,23 @@ def extract_labels(data):
 dataset = pd.read_csv(DATA_ROOT + 'spam.csv')
 train_data, train_labels = extract_labels(dataset)
 
+
+# Split train and test data
+train_data, test_data, train_labels, test_labels = train_test_split(train_data, train_labels, test_size=0.2)
+
 # Extract features using Bag of Words model
 count_vect = CountVectorizer()
-X_train = count_vect.fit_transform(train_data)
+train_data = count_vect.fit_transform(train_data)
 
 # Enable TF-IDF
 tf_idf = TfidfTransformer()
-X_train = tf_idf.fit_transform(X_train)
-print(X_train.shape)
+train_data = tf_idf.fit_transform(train_data)
+
+# Train the Naive Bayes classifier
+clf = MultinomialNB().fit(train_data, train_labels)
+
+
+
 
 
 
